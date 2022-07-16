bb966e6e7121f176c0193a244c19f9902eddb609
Adithya Anandsaikrishnan <anandsaikrishnan.adithya@gmail.com>
2022-07-16 00:47:07 -0700

    set up initial project files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.scss'
+
+// Todos 
+// Projects
+// View / UI
+// Button interacts with tools
+// Projects implement notes and to-dos
+// Notes are any text (REMEMBER TO SANITIZE)
+// To dos are title,  due date, color/importance
+// Expanding & Deleting To Dos
+// Storing To Do Lists on Local Storage 
